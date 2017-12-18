a9592d9d48a7cce8affd47c976feb48e6c085a6e
Kirill Makhonin <kroks.rus@gmail.com>
2017-12-19 01:58:23 +0300

    Add toasts about DB creation, disable common tests project

--- Keeeys/Keeeys.Droid/Activities/BaseActivity.cs
+++ Keeeys/Keeeys.Droid/Activities/BaseActivity.cs
@@ -6,6 +6,7 @@ using Keeeys.Droid.PlatformSpecific;
 using Keeeys.Common.Models;
 using SQLite.Net;
 using SQLite.Net.Platform.XamarinAndroid;
+using Android.Widget;
 
 namespace Keeeys.Droid.Activities
 {
@@ -34,28 +35,41 @@ namespace Keeeys.Droid.Activities
         private static bool connectedToDatabase = false;
         private void CheckAndConnectToDatabase()
         {
-            if (!connectedToDatabase)
+            try
             {
-                string databasePath = FileHelper.GetLocalFilePath(dbName);
-                /// TODO: Database file removal
-                //FileHelper.RemoveFile(databasePath);
-
-                Android.Util.Log.Info("DB", "DB file {0}: {1}", databasePath, FileHelper.IsFileExists(databasePath) ? "exists" : "not exists");
-
-                try
+                if (!connectedToDatabase)
                 {
-                    DataProvider.Connection = new SQLiteConnection(new SQLitePlatformAndroid(), databasePath);
+                    string databasePath = FileHelper.GetLocalFilePath(dbName);
+
+                    if (!FileHelper.IsFileExists(dbName))
+                    {
+                        Toast.MakeText(this, "База данных отсутствует", ToastLength.Short).Show();
+                    }
+
+                    /// TODO: Database file removal
+                    //FileHelper.RemoveFile(databasePath);
+
+                    Android.Util.Log.Info("DB", "DB file {0}: {1}", databasePath, FileHelper.IsFileExists(databasePath) ? "exists" : "not exists");
+
+                    try
+                    {
+                        DataProvider.Connection = new SQLiteConnection(new SQLitePlatformAndroid(), databasePath);
+                    }
+                    catch (Exception databaseConnectionException)
+                    {
+                        Android.Util.Log.Error("DB", "Fail to create connection: {0}", databaseConnectionException);
+                    }
+
+                    Android.Util.Log.Info("DB", "Database path: {0}", DataProvider.Connection.DatabasePath);
+
+                    connectedToDatabase = true;
+
+                    DataProvider.Get();
                 }
-                catch (Exception databaseConnectionException)
-                {
-                    Android.Util.Log.Error("DB", "Fail to create connection: {0}", databaseConnectionException);
-                }
-
-                Android.Util.Log.Info("DB", "Database path: {0}", DataProvider.Connection.DatabasePath);
-
-                connectedToDatabase = true;
-
-                DataProvider.Get();
+            }
+            catch(Exception dbException)
+            {
+                Toast.MakeText(this, "База данных установлена!", ToastLength.Short).Show();
             }
         }
 
