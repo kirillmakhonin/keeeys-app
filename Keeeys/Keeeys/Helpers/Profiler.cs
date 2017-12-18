04ab43591b04e73f3f0787145480d5e59f5624af
Kirill Makhonin <kroks.rus@gmail.com>
2017-12-19 00:53:37 +0300

    Add profiler

---
+++ Keeeys/Keeeys/Helpers/Profiler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Keeeys.Common.Helpers
+{
+    public class Profiler : IDisposable
+    {
+        private Stopwatch stopwatch;
+        private string name;
+
+        public Profiler(string name)
+        {
+#if DEBUG
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            this.name = name;
+#endif
+        }
+
+        public void Dispose()
+        {
+#if DEBUG
+            stopwatch.Stop();
+            Debug.WriteLine(String.Format("PROFILING:: {0}: {1}", this.name, stopwatch.Elapsed));
+#endif
+        }
+    }
+}
